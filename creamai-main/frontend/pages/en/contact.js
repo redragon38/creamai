46705edaec3c322c1f282f2183fea942397402a1
redragon38 <l.bonin2011@gmail.com>
2026-02-28 12:27:41 +0100

    aa

--- creamai-main/frontend/pages/en/contact.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-export default function Redirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/contact'); }, []);
-  return null;
-}
